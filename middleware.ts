accdf0e05536f784896a3ba878a9220df83df26b
s6852d10028-star <s6852d10028@sau.ac.th>
2026-07-11 23:07:36 +0700

    v2

--- middleware.ts
+++ middleware.ts
@@ -26,3 +26,9 @@ export async function middleware(request: NextRequest) {
 
   return response
 }
+
+export const config = {
+  matcher: [
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+  ],
+}
